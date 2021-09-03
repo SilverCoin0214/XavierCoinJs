57f8736199152e656f46c5b39a2c22ce22749ae0
Spike <zhangzuoxuan@gmail.com>
2021-09-03 10:39:21 +0800

    new

---
+++ Js_Advanced/02_cache.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-03 10:39:00
+ * @LastEditTime: 2021-09-03 10:39:02
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /XavierCoinJs/Js_Advanced/02_cache.js
+ */
